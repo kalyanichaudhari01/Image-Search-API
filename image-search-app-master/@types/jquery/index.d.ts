55ee95ca970070832b2b03527429c5e863d06ca6
KalyaniChaudhari01 <kalyanichaudhari937@gmail.com>
2025-06-09 14:29:07 +0530

    file addedd

---
+++ image-search-app-master/@types/jquery/index.d.ts
@@ -0,0 +1,8 @@
+
+/// <reference types="sizzle" />
+/// <reference path="JQueryStatic.d.ts" />
+/// <reference path="JQuery.d.ts" />
+/// <reference path="misc.d.ts" />
+/// <reference path="legacy.d.ts" />
+
+export = jQuery;
